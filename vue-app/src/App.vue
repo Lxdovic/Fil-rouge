e704f4b6f1f34c692650799237f127e8f2364471
Ludovic <80395681+Lxdovic@users.noreply.github.com>
2022-02-21 12:04:53 +0100

    Update

---
+++ vue-app/src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+  import Nav from './components/Nav.vue'
+  import Menu from './components/Menu.vue'
+  import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  <Nav/>
+  <Menu/>
+  <Footer/>
+</template>
+
+<style>
+  * {
+    overflow-x: hidden;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  ::-webkit-scrollbar {
+    width: 9px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: #d8d8d8;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: #888;
+    width: 7px;
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background: rgb(160, 160, 160);
+  }
+</style>
---
+++ vue-app/src/components/Menu.vue
@@ -0,0 +1,163 @@
+<script setup>
+  import { ref } from 'vue'
+
+  defineProps({
+    msg: String
+  })
+
+  const count = ref(0)
+</script>
+
+<template>
+  <div class='bg-gray-100 w-screen h-auto py-10'>
+    <h1 class='font-bold mb-2 text-xl mb-10'>Play</h1>
+
+    <div class='flex justify-center flex-wrap'>
+      <div class='container w-64 h-32'>
+        <button class='py-2 px-3 w-40 bg-gray-200 rounded'>Online</button>
+        <p class='pt-6 text-gray-400'>Play online with other players </p>
+      </div>
+      
+      <div class='container w-64 h-32'>
+        <button class='py-2 px-3 w-40 bg-gray-200 rounded'>With a friend</button>
+        <p class='pt-6 text-gray-400'>Play with your friends</p>
+      </div>
+      
+      <div class='container w-64 h-32'>
+        <button class='py-2 px-3 w-40 bg-gray-200 rounded'>With the computer</button>
+        <p class='pt-6 text-gray-400'>Play against the computer</p>
+      </div>
+    </div>
+  </div>
+
+  <div class='bg-white w-screen h-auto pt-10 flex justify-evenly flex-wrap px-10 mb-20'>
+    <div class='mb-10'>
+      <h1 class='font-bold mb-2 text-xl'>News</h1>
+      <div class='container bg-[#30302E] w-[700px] h-96 rounded overflow-y-scroll'>
+        <div>
+
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <h1 class='font-bold mb-2 text-xl'>History</h1>
+      <div class='container bg-[#30302E] w-[700px] h-96 rounded overflow-y-scroll'>
+        <div class='m-auto w-full pt-2 pb-2 pl-10 bg-[#41413E] flex justify-left border-gray-500 border-b'>
+          <span class='bg-[#E83943] rounded mr-4 w-3 h-3 mt-1'></span>
+          <span class='text-gray-200 mr-4'>3h</span>
+          <span class='text-gray-400 mr-4 float-right'>(901 - 11)</span>
+          <span class='text-gray-200 mr-4'>Rapid</span>
+          <span class='text-gray-200 float-right'>Player 1</span>
+        </div>
+
+        <div class='m-auto w-full pt-2 pb-2 pl-10 bg-[#41413E] flex justify-left border-gray-500 border-b'>
+          <span class='bg-[#36B18A] rounded mr-4 w-3 h-3 mt-1'></span>
+          <span class='text-gray-200 mr-4'>3h</span>
+          <span class='text-gray-400 mr-4 float-right'>(901 + 12)</span>
+          <span class='text-gray-200 mr-4'>Rapid</span>
+          <span class='text-gray-200 float-right'>Player 1</span>
+        </div>
+
+        <div class='m-auto w-full pt-2 pb-2 pl-10 bg-[#41413E] flex justify-left border-gray-500 border-b'>
+          <span class='bg-[#36B18A] rounded mr-4 w-3 h-3 mt-1'></span>
+          <span class='text-gray-200 mr-4'>3h</span>
+          <span class='text-gray-400 mr-4 float-right'>(901 + 12)</span>
+          <span class='text-gray-200 mr-4'>Rapid</span>
+          <span class='text-gray-200 float-right'>Player 1</span>
+        </div>
+
+        <div class='m-auto w-full pt-2 pb-2 pl-10 bg-[#41413E] flex justify-left border-gray-500 border-b'>
+          <span class='bg-[#E83943] rounded mr-4 w-3 h-3 mt-1'></span>
+          <span class='text-gray-200 mr-4'>3h</span>
+          <span class='text-gray-400 mr-4 float-right'>(901 - 13)</span>
+          <span class='text-gray-200 mr-4'>Rapid</span>
+          <span class='text-gray-200 float-right'>Player 1</span>
+        </div>
+
+        <div class='m-auto w-full pt-2 pb-2 pl-10 bg-[#41413E] flex justify-left border-gray-500 border-b'>
+          <span class='bg-[#36B18A] rounded mr-4 w-3 h-3 mt-1'></span>
+          <span class='text-gray-200 mr-4'>3h</span>
+          <span class='text-gray-400 mr-4 float-right'>(901 + 12)</span>
+          <span class='text-gray-200 mr-4'>Rapid</span>
+          <span class='text-gray-200 float-right'>Player 1</span>
+        </div>
+
+        <div class='m-auto w-full pt-2 pb-2 pl-10 bg-[#41413E] flex justify-left border-gray-500 border-b'>
+          <span class='bg-[#36B18A] rounded mr-4 w-3 h-3 mt-1'></span>
+          <span class='text-gray-200 mr-4'>4h</span>
+          <span class='text-gray-400 mr-4 float-right'>(901 + 9)</span>
+          <span class='text-gray-200 mr-4'>Rapid</span>
+          <span class='text-gray-200 float-right'>Player 1</span>
+        </div>
+
+        <div class='m-auto w-full pt-2 pb-2 pl-10 bg-[#41413E] flex justify-left border-gray-500 border-b'>
+          <span class='bg-[#36B18A] rounded mr-4 w-3 h-3 mt-1'></span>
+          <span class='text-gray-200 mr-4'>4h</span>
+          <span class='text-gray-400 mr-4 float-right'>(901 + 12)</span>
+          <span class='text-gray-200 mr-4'>Rapid</span>
+          <span class='text-gray-200 float-right'>Player 1</span>
+        </div>
+
+        <div class='m-auto w-full pt-2 pb-2 pl-10 bg-[#41413E] flex justify-left border-gray-500 border-b'>
+          <span class='bg-[#E83943] rounded mr-4 w-3 h-3 mt-1'></span>
+          <span class='text-gray-200 mr-4'>5h</span>
+          <span class='text-gray-400 mr-4 float-right'>(901 - 9)</span>
+          <span class='text-gray-200 mr-4'>Rapid</span>
+          <span class='text-gray-200 float-right'>Player 1</span>
+        </div>
+
+        <div class='m-auto w-full pt-2 pb-2 pl-10 bg-[#41413E] flex justify-left border-gray-500 border-b'>
+          <span class='bg-[#E83943] rounded mr-4 w-3 h-3 mt-1'></span>
+          <span class='text-gray-200 mr-4'>5h</span>
+          <span class='text-gray-400 mr-4 float-right'>(901 - 11)</span>
+          <span class='text-gray-200 mr-4'>Rapid</span>
+          <span class='text-gray-200 float-right'>Player 1</span>
+        </div>
+
+        <div class='m-auto w-full pt-2 pb-2 pl-10 bg-[#41413E] flex justify-left border-gray-500 border-b'>
+          <span class='bg-[#E83943] rounded mr-4 w-3 h-3 mt-1'></span>
+          <span class='text-gray-200 mr-4'>5h</span>
+          <span class='text-gray-400 mr-4 float-right'>(901 - 11)</span>
+          <span class='text-gray-200 mr-4'>Rapid</span>
+          <span class='text-gray-200 float-right'>Player 1</span>
+        </div>
+        
+        <div class='m-auto w-full pt-2 pb-2 pl-10 bg-[#41413E] flex justify-left border-gray-500 border-b'>
+          <span class='bg-[#E83943] rounded mr-4 w-3 h-3 mt-1'></span>
+          <span class='text-gray-200 mr-4'>6h</span>
+          <span class='text-gray-400 mr-4 float-right'>(901 - 11)</span>
+          <span class='text-gray-200 mr-4'>Rapid</span>
+          <span class='text-gray-200 float-right'>Player 1</span>
+        </div>
+
+        <div class='m-auto w-full pt-2 pb-2 pl-10 bg-[#41413E] flex justify-left border-gray-500 border-b'>
+          <span class='bg-[#36B18A] rounded mr-4 w-3 h-3 mt-1'></span>
+          <span class='text-gray-200 mr-4'>12h</span>
+          <span class='text-gray-400 mr-4 float-right'>(901 + 11)</span>
+          <span class='text-gray-200 mr-4'>Rapid</span>
+          <span class='text-gray-200 float-right'>Player 1</span>
+        </div>
+        
+        <div class='m-auto w-full pt-2 pb-2 pl-10 bg-[#41413E] flex justify-left border-gray-500 border-b'>
+          <span class='bg-[#E83943] rounded mr-4 w-3 h-3 mt-1'></span>
+          <span class='text-gray-200 mr-4'>12h</span>
+          <span class='text-gray-400 mr-4 float-right'>(901 - 11)</span>
+          <span class='text-gray-200 mr-4'>Rapid</span>
+          <span class='text-gray-200 float-right'>Player 1</span>
+        </div>
+        
+        <div class='m-auto w-full pt-2 pb-2 pl-10 bg-[#41413E] flex justify-left border-gray-500 border-b'>
+          <span class='bg-[#E83943] rounded mr-4 w-3 h-3 mt-1'></span>
+          <span class='text-gray-200 mr-4'>14h</span>
+          <span class='text-gray-400 mr-4 float-right'>(901 - 10)</span>
+          <span class='text-gray-200 mr-4'>Rapid</span>
+          <span class='text-gray-200 float-right'>Player 1</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
